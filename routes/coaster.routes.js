5f37c4262afedd251ca830f548276cf742e098f3
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-02-12 12:27:56 +0100

    hecho hasta el punto 14

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -1,7 +1,65 @@
 const express = require('express')
 const router = express.Router()
 
+const Coaster = require('../models/coaster.model')
+const Park = require('../models/park.model')
+
 // Aquí los endpoints
 
+//Crear
+router.get("/new", (req, res, next) => {
+    Park.find({})
+        .then(parks => res.render("coasters/new-coaster", {
+            parks
+        }))
+        .catch(err => {
+            console.log("Hubo un error, ya no se que poner", err)
+        });
+});
+
+router.post("/new", (req, res, next) => {
+
+    const {
+        name,
+        description,
+        inversions,
+        length,
+        active,
+        park
+    } = req.body
+
+    Coaster.create({
+            name,
+            description,
+            inversions,
+            length,
+            active,
+            park,
+        })
+        .then(() => res.redirect("/coasters/new"))
+        .catch(err => {
+            console.log("Hubo un fallo creando newCoaster", err)
+        });
+})
+
+router.get("/", (req, res, next) => {
+    Coaster.find({}).populate("park")
+        .then(coasters => res.render("coasters/coasters-index", {
+            coasters
+        }))
+        .catch(err => {
+            console.log("Hubo un erroooooor", err)
+        });
+});
+
+router.get("/:id", (req, res, next) => {
+    Coaster.findById(req.params.id).populate("park")
+        .then(coaster => {
+            res.render("coasters/coaster-details", {
+                coaster
+            })
+        })
+        .catch(err => console.log("hay un error en el listado", err))
+});
 
 module.exports = router
--- routes/park.routes.js
+++ routes/park.routes.js
@@ -1,6 +1,30 @@
 const express = require('express')
 const router = express.Router()
 
+const Park = require("../models/park.model");
+
+
 // Aquí los endpoints
+
+router.get('/new', (req, res) => res.render('parks/new-park'))
+
+router.post('/new', (req, res) => {
+
+    const {
+        name,
+        description,
+        active
+    } = req.body
+
+    Park.create({
+            name,
+            description,
+            active
+        })
+        .then(() => res.redirect('/parks/new'))
+        .catch(err => {
+            console.log("Hubo un fallo añadiendo newPark", err)
+        })
+});
 
 module.exports = router
